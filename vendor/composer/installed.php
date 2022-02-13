7e492c80156ee8287c98a3f1aab56656edf7b845
Matheus Aguiar <matheus.aguiar@apiki.com>
2022-02-13 12:28:36 -0300

    build: building plugin

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '341c22d57fd5fa48e01bd3869fec2193da700dbe',
+    'name' => 'aguiarrdev/search-tables',
+  ),
+  'versions' => 
+  array (
+    'aguiarrdev/search-tables' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '341c22d57fd5fa48e01bd3869fec2193da700dbe',
+    ),
+  ),
+);
